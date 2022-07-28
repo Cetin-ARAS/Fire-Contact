1b1a0dcb5c6dee83254a237eee13a1042f726047
ÇETİN <cetinnnarasss@gmail.com>
2022-07-28 22:24:19 +0300

    cont.

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,14 +1,42 @@
 import firebase from "./firebase"
-import { getDatabase, ref, push, set } from "firebase/database";
+import { getDatabase,onValue,push, ref, set  } from "firebase/database";
+import {useState,useEffect} from "react"
 
 
-export const AddUser = (info) => {
-
+// Bilgi Ekleme
+export const AddUser=(info)=>{
     const db = getDatabase(firebase);
-    const userRef = ref(db, "users/")
-    const newUserRef = push(userRef)
-    set(newUserRef, {
-        username:info.username, 
-        phoneNumber: info.number, 
-        gender: info.gender})
+    const userRef=ref(db,"users/")
+    const newUserRef=push(userRef);
+    set(newUserRef,{
+        username:info.username,
+        phoneNumber:info.phoneNumber,
+        gender:info.gender
+    })
+
+}
+
+// Bilgi Çağırma
+
+export const useFetch=()=>{
+     const [isLoading,setIsLoading]=useState();
+     const [contactList,setContactList]=useState();
+    useEffect(() => {
+        const db = getDatabase(firebase);
+        const userRef=ref(db,"users/")
+        onValue(userRef,(snapshot)=>{
+            const data=snapshot.val();
+            const userArray=[]
+
+            for (let id in data){
+                userArray.push({id,...data[id]})
+            }
+            setContactList(userArray)
+            setIsLoading(false)
+        })
+    },[])
+    return {isLoading,contactList}
+
+
+
 }
